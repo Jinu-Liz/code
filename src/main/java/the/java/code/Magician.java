98d6ee3678d5617b301d4772007f246a7a1e6b3b
hsyoon <bagzein@gmail.com>
2024-05-01 19:19:56 +0900

    - 바이트코드 조작 > 차례대로 실행시키지 않아도 동작하도록 ClassLoader를 사용하여 수정

--- src/main/java/the/java/code/Magician.java
+++ src/main/java/the/java/code/Magician.java
@@ -1,7 +1,9 @@
 package the.java.code;
 
 import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.dynamic.ClassFileLocator;
 import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.pool.TypePool;
 
 import java.io.File;
 import java.io.IOException;
@@ -10,8 +12,13 @@ import static net.bytebuddy.matcher.ElementMatchers.named;
 
 public class Magician {
   public static void main(String[] args) {
+    ClassLoader classLoader = Magician.class.getClassLoader();
+    TypePool typePool = TypePool.Default.of(classLoader);
     try {
-      new ByteBuddy().redefine(Moja.class)
+      new ByteBuddy().redefine(
+        typePool.describe("the.java.code.Moja").resolve(),
+          ClassFileLocator.ForClassLoader.of(classLoader)
+        )
         .method(named("pullOut"))
         .intercept(FixedValue.value("Rabbit!"))
         .make()
@@ -21,6 +28,6 @@ public class Magician {
       e.printStackTrace();
     }
 
-//    System.out.println(new Moja().pullOut());
+    System.out.println(new Moja().pullOut());
   }
 }
