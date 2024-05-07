ca506ff971e54315961a419a5a535e6bdc1796bb
Hyungseock-Yun <bagzein@gmail.com>
2024-05-07 23:38:15 +0900

    - 리플렉션 > 스프링 DI 예시

--- src/main/java/the/java/code/book/BookService.java
+++ src/main/java/the/java/code/book/BookService.java
@@ -1,7 +1,12 @@
 package the.java.code.book;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookService {
+
+  @Autowired
+  BookRepository bookRepository;
+
 }
